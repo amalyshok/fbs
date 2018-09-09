5dbbcec33f1e8d98caf6418416ac65535d41e531
Aleks M <amalyshok@mail.ru>
2018-09-10 00:31:00 +0300

    шаблоны+ бодипарсер+

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 // ./index.js
 let express = require('express');
-var bodyParser = require('body-parser');
+let bodyParser = require('body-parser');
 let app = express();
 let http = require('http').Server(app);
-let cors = require('cors');
 require('./static/socket/serverSocket')(http);
+app.set('view engine', 'ejs');
 
-app.use('/', function(req, res, next) {
-	console.log('Путь: ' + req.url);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+//тестовый обработчик json post запроса (работает ыыыы =)
+app.use('/test', function(req, res, next) {
+	console.log(req.body);
 	next();
 });
- 
+
+// рендерим шаблон основной страницы сайта, с обращением по имени. 
+app.use('/template', function (req, res, next) {
+	res.render( 'index', {name: 'Александр'} );
+	next();
+});
+
 app.use(express.static(__dirname + '/static'));
 
 http.listen(3000, () => console.log('Http started on port: 3000'));
--- static/socket/cloneSocket.js
+++ static/socket/cloneSocket.js
@@ -1,22 +1,3 @@
            	socket.emit('Hi', 'Привет, сервер, я клон сайта');
 			socket.on('Hi', (data) => {console.log(data)});
 			socket.on('disconnect', () => console.log('Сервер закрыл соединение'));
-			function sendText() {
-				let text = document.querySelector('#text').value;
-				socket.emit('string', text);
-				} 
-
-if (document.querySelector('#text')) {
-	sendText();
-input.onchange = function () {
-	
-	input.blur();
-	};	
-	
-input.onfocus = function () {
-	input.value = '';
-	};
-}
-
-
-	 
